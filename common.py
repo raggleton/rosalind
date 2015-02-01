7940d30d0913de2f9e07dae02f715e171c31004c
Robin Aggleton <robinaggleton@gmail.com>
2015-02-02 00:15:39 +0100

    fix IndexError, screen niceness

--- common.py
+++ common.py
@@ -2,19 +2,22 @@ import sys
 
 
 def runit(custom_fn):
-    if not sys.argv[1]:
+    if len(sys.argv) == 1:
         print "Not running, need filename"
         return
     print sys.argv[1]
     with open(sys.argv[1], "r") as f:
         line = f.readline().strip()
+        print "IN: "
         print line
         out = custom_fn(line)
+        print "OUT: "
         print out
         return out
 
 
 def test(custom_fn, sample_data, sample_output):
+    print sample_data
     if sample_output != custom_fn(sample_data):
         print "YOU MUCKED UP"
         return False

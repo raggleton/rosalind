a57f2be42be8bb4e1b82239c6ca25ad30fcb5e64
Robin Aggleton <robinaggleton@gmail.com>
2015-02-01 23:00:53 +0100

    More solutions

--- counting_nucleotides.py
+++ counting_nucleotides.py
@@ -12,17 +12,30 @@ sample_data = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATA
 sample_output = "20 12 17 21"
 
 
-def counter(word):
+def counter_old(word):
     """brute-force way"""
     counter = dict(A=0, C=0, G=0, T=0)
     for w in word:
-        if w in alphabet:
+        if w in alphabet:  # error checking
             counter[w] += 1
     print counter["A"], counter["C"], counter["G"], counter["T"]
     return "%s %s %s %s" % (counter["A"], counter["C"], counter["G"], counter["T"])
 
+def counter_good(word):
+    """nice way"""
+    counter = dict((x, word.count(x)) for x in alphabet)
+    print counter["A"], counter["C"], counter["G"], counter["T"]
+    return "%s %s %s %s" % (counter["A"], counter["C"], counter["G"], counter["T"])
 
-out = counter(sample_data)
+def counter_fancy(word):
+    """super nice way"""
+    from collections import Counter
+    counter = Counter(word)
+    print counter["A"], counter["C"], counter["G"], counter["T"]
+    return "%s %s %s %s" % (counter["A"], counter["C"], counter["G"], counter["T"])
+
+
+out = counter_fancy(sample_data)
 if out != sample_output:
     print "YOU MUCKED UP"
     print out
